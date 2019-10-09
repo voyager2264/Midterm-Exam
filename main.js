e0b6a7ea02388ee06b291932181166d347d930b0
Peter Brown <peter.brown@converse.edu>
2019-10-09 13:25:40 -0400

    Made code reflect exam questions

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-// The TabWidget class includes code derived from WAI-ARIA examples:
-// This software or document includes material copied from or derived from WAI-ARIA
-// Authoring Practices 1.1, Example of Tabs with Automatic Activation.
-// https://www.w3.org/TR/wai-aria-practices/examples/tabs/tabs-1/tabs.html
-// Copyright © 2018 W3C® (MIT, ERCIM, Keio, Beihang).
 
 window.onload = () => {
-    
+    // Enable ChromeVox
+    document.getElementById('enable-cvox').click();
 }
 
 function showWeather() {
