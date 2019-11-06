024651f836f1158449c7cd1d466269fce42ada74
Shelby Craig <sacraig001@cs0.converse.edu>
2019-11-06 14:54:35 -0500

    my answers

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ window.onload = () => {
 }
 
 function showWeather() {
+    let GetWeather = function(req) {
+	let data = JSON.parse(req.responseText);
+	document.getElementById("temp").innerHTML=data.main.temp;
+	document.getElementById("humidity").innerHTML=data.main.humidity;
+    };
+    handleRequestOnCompletion(GetWeather, "/~phbrown/CSC335/weather.php");
 }
